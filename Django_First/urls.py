b566b8899dfdd64088c2bcb4d3961c6a2b3b83ca
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-08-31 17:35:53 +0500

    From urls comment deleted

--- Django_First/urls.py
+++ Django_First/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.urls import path
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('Music/', include('Music.urls')),
+    path("admin/", admin.site.urls),
+    path("music/", include("Music.urls"))
+    #url(r'^admin/', admin.site.urls),
+    #url('Music/', include('Music.urls')),
 ]
