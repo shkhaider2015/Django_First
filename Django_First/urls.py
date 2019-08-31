f30e02ca9789e0c6246a04c1fe68f9b1de484922
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-08-31 18:45:48 +0500

    From urls comment deleted

--- Django_First/urls.py
+++ Django_First/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
-from django.conf.urls import include, url
-from django.urls import path
+#from django.conf.urls import include, url
+from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
