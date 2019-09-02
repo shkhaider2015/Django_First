6fd76c1cdebcd19027a474b612d589fa3211d2a9
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-09-02 16:33:00 +0500

    Creating Database
    Signed-off-by: Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>

--- Django_First/settings.py
+++ Django_First/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Music.apps.MusicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
