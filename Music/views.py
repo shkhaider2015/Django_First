1ff4e87a762d13bb228e82c9d94a5bf74256b85f
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-09-03 12:38:18 +0500

    add folder and HTML file
    Signed-off-by: Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>

--- Music/views.py
+++ Music/views.py
@@ -3,11 +3,14 @@ from django.template import loader
 from .models import Album
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template("")
+    template = loader.get_template("music/index.html")
+    context = {
+        'all_albums': all_albums,
+    }
     # html = ""
     # for album in all_albums:
     #     url = "/music/" + str(album.id) + "/"
     #     html += "<a href=" + url + ">" + album.album_tittle + "</a><br>"
-    return HttpResponse()
+    return HttpResponse(template.render(context, request))
 def detail(request, album_id):
-    return HttpResponse("<h2>Details For Album id:"+ str(album_id)+" </h2>")
+    return HttpResponse("<h2>Details For Album id:"+str(album_id)+" </h2>")
