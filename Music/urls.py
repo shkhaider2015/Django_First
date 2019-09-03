baf7f9af83e70351b5d288d54a5aa0ed8fa1096e
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-09-03 09:54:27 +0500

    Creating Another View
    Signed-off-by: Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>

--- Music/urls.py
+++ Music/urls.py
@@ -3,5 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+# /Music/
     path('', views.index, name='index'),
+# /Music/729/
+    # url(r"^(?P<album_id>[0-9] + )$", view.detail, name="detail")
+    path("<int:album_id>/", views.detail, name="detail")
 ]
--- Music/views.py
+++ Music/views.py
@@ -1,3 +1,5 @@
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>This is Music App Home Page</h1>")
+def detail(request, album_id):
+    return HttpResponse("<h2>Details For Album id:"+ str(album_id)+" </h2>")
