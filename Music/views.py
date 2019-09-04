99ea67c7137a2f36997afeedc66088dbb950174c
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-09-04 13:40:35 +0500

    Raising Http 404 Page
    Signed-off-by: Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>

--- Music/views.py
+++ Music/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
+
 def index(request):
     all_albums = Album.objects.all()
     context = {'all_albums': all_albums}
-    # html = ""
-    # for album in all_albums:
-    #     url = "/music/" + str(album.id) + "/"
-    #     html += "<a href=" + url + ">" + album.album_tittle + "</a><br>"
     return render(request, "music/index.html", context)
+
 def detail(request, album_id):
-    return HttpResponse("<h2>Details For Album id:"+str(album_id)+" </h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does Not Exist")
+    return render(request, "", {"album": album})
